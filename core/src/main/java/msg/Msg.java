43c5bf3ba1c16cf869fa9d3faad5710191900ef7
qq5132834 <513283439@qq.com>
2022-06-05 16:46:13 +0800

    m

--- core/src/main/java/msg/Msg.java
+++ core/src/main/java/msg/Msg.java
@@ -1,33 +1,42 @@
 package msg;
 
-import com.sun.javafx.binding.StringFormatter;
-
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class Msg {
 
-    public static String getValue(Class cls, String path, String key){
+    public static String getValue(String path, String key){
         key = key.replaceAll("_", ".");
         ResourceBundle rb = ResourceBundle.getBundle(path);
         String val = rb.getString(key);
-        System.out.println(cls.getSimpleName() + "-" + "ResourceBundle:" + rb.getLocale().getCountry() + "/" + rb.getLocale().getLanguage());
-        System.out.println(cls.getSimpleName() + "-" + "path:" + path);
-        System.out.println(cls.getSimpleName() + "-" + "key:" + key + ",val:" + val);
+        System.out.println("ResourceBundle:" + rb.getLocale().getCountry() + "/" + rb.getLocale().getLanguage());
+        System.out.println("path:" + path);
+        System.out.println("key:" + key + ",val:" + val);
         return val;
     }
 
-    public static String getValue(Class cls, String path, String key, String... vs){
+    public static String getValue(Package pack, String path, String key){
+        String str = pack.getName().replaceAll("\\.", "/");
+        path = str + "/" + path;
+        return getValue(path, key);
+    }
+
+    public static String getFormatValue(String path, String key, String... vs){
         final StringBuilder stringBuilder = new StringBuilder();
         Arrays.stream(vs).forEach(e->stringBuilder.append(e));
         key = key.replaceAll("_", ".");
         ResourceBundle rb = ResourceBundle.getBundle(path);
         String val = rb.getString(key);
         val = val + " " +  stringBuilder.toString();
-        System.out.println(cls.getSimpleName() + "-" + "ResourceBundle:" + rb.getLocale().getCountry() + "/" + rb.getLocale().getLanguage());
-        System.out.println(cls.getSimpleName() + "-" + "path:" + path);
-        System.out.println(cls.getSimpleName() + "-" + "key:" + key + ",val:" + val);
+        System.out.println("ResourceBundle:" + rb.getLocale().getCountry() + "/" + rb.getLocale().getLanguage());
+        System.out.println("path:" + path);
+        System.out.println("key:" + key + ",val:" + val);
         return val + " " +  stringBuilder.toString();
+    }
+
+    public static String getFormatValue(Package pack, String path, String key, String... vs){
+        path = pack.getName().replaceAll("\\.", "/") + "/" + path;
+        return getFormatValue(path, key, vs);
     }
 
 }
